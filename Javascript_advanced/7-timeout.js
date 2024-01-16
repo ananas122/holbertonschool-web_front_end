bfed51ee896517ecc869e970fbb269bff1831e24
ananas122 <122387449+ananas122@users.noreply.github.com>
2024-01-16 20:22:35 +0100

    Stack order and setTimeout

--- Javascript_advanced/7-timeout.js
+++ Javascript_advanced/7-timeout.js
@@ -4,7 +4,7 @@ setTimeout(function() {
     console.log("Final code block to be executed");
 }, 0);
 //Using a loop that iterates 100 times, each iteration logs to the console the iteration number
-for (var i = 1; i <= 100; i++) {
+for (let i = 1; i <= 100; i++) {
         console.log("Itération numéro :", i);
 }
 
