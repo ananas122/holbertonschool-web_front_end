b78ffeecd9aa8ee01fbb17e1c52f15cf89a4d64c
ananas122 <122387449+ananas122@users.noreply.github.com>
2024-01-16 20:32:09 +0100

    Stack order and setTimeout

--- Javascript_advanced/7-timeout.js
+++ Javascript_advanced/7-timeout.js
@@ -5,7 +5,7 @@ setTimeout(function() {
 }, 0);
 //Using a loop that iterates 100 times, each iteration logs to the console the iteration number
 for (let i = 1; i <= 100; i++) {
-        console.log("Itération numéro :", i);
+        console.log(i);
 }
 
 console.log("End of the loop printing");
