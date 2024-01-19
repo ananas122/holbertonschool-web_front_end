de3c3f2cbf869521912df5adb65d5995db0934f2
ananas122 <122387449+ananas122@users.noreply.github.com>
2024-01-19 14:50:10 +0100

    prime

---
+++ Javascript_advanced/9-prime.js
@@ -0,0 +1,35 @@
+// Déclaration de la fonction countPrimeNumbers pour compter les nombres premiers
+function countPrimeNumbers() {
+  let count = 0;  // Initialisation du compteur de nombres premiers à 0
+  let prime;  // Variable pour déterminer si un nombre est premier ou non
+
+  // Boucle principale: vérifier chaque nombre de 2 à 100
+  for (let i = 2; i <= 100; i++) {
+    prime = true;  // On suppose initialement que le nombre est premier
+
+    // Vérifier si 'i' peut être divisé par un nombre inférieur à lui-même
+    for (let j = 2; j < i; j++) {
+      // Si 'i' est divisible par 'j', 'i' n'est pas premier
+      if (i % j == 0) {
+        prime = false;  // 'i' n'est pas un nombre premier
+        break;  // Arrêter la vérification pour le nombre actuel 'i' car il n'est pas premier
+      }
+    }
+    
+    // Si 'i' est premier (prime est toujours vrai), incrémenter le compteur
+    if (prime) {
+      count += 1;
+    }
+  }
+  return count;  // Retourner le nombre total de nombres premiers trouvés
+}
+
+// Mesurer le temps d'exécution de la fonction countPrimeNumbers
+const t0 = performance.now();  // Heure de début
+countPrimeNumbers();  // Exécuter la fonction countPrimeNumbers
+const t1 = performance.now();  // Heure de fin
+
+// Afficher le temps d'exécution dans la console
+console.log(
+  `Execution time of printing countPrimeNumbers was ${t1 - t0} milliseconds.`
+);
